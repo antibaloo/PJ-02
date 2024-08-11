7182eda70a89819495acc047d1c932d1a869555f
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-08-11 17:10:21 +0500

    Изменена струтура хранилища в памяти. Изменен конструктор этой структуры.

--- pkg/storage/memdb/memdb.go
+++ pkg/storage/memdb/memdb.go
@@ -3,14 +3,25 @@ package memdb
 import "GoNews/pkg/storage"
 
 // Хранилище данных.
-type Store struct{}
+type Store struct {
+	posts        map[int]string
+	authors      map[int]string
+	lastPostID   int
+	lastAuthorID int
+}
 
 // Конструктор объекта хранилища.
 func New() *Store {
-	return new(Store)
+	return &Store{
+		posts:        make(map[int]string),
+		authors:      make(map[int]string),
+		lastPostID:   0,
+		lastAuthorID: 0,
+	}
 }
 
 func (s *Store) Posts() ([]storage.Post, error) {
+	var posts []storage.Post
 	return posts, nil
 }
 
@@ -22,17 +33,4 @@ func (s *Store) UpdatePost(storage.Post) error {
 }
 func (s *Store) DeletePost(storage.Post) error {
 	return nil
-}
-
-var posts = []storage.Post{
-	{
-		ID:      1,
-		Title:   "Effective Go",
-		Content: "Go is a new language. Although it borrows ideas from existing languages, it has unusual properties that make effective Go programs different in character from programs written in its relatives. A straightforward translation of a C++ or Java program into Go is unlikely to produce a satisfactory result—Java programs are written in Java, not Go. On the other hand, thinking about the problem from a Go perspective could produce a successful but quite different program. In other words, to write Go well, it's important to understand its properties and idioms. It's also important to know the established conventions for programming in Go, such as naming, formatting, program construction, and so on, so that programs you write will be easy for other Go programmers to understand.",
-	},
-	{
-		ID:      2,
-		Title:   "The Go Memory Model",
-		Content: "The Go memory model specifies the conditions under which reads of a variable in one goroutine can be guaranteed to observe values produced by writes to the same variable in a different goroutine.",
-	},
 }
