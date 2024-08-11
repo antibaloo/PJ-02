58f7436117c98e2992b35dbc133bcfa24c77a17b
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-08-11 17:02:24 +0500

    Делаем три точки входа в приложение для разных типов хранилища данных

---
+++ cmd/server_mongo/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"GoNews/pkg/api"
+	"GoNews/pkg/storage"
+	"GoNews/pkg/storage/mongodb"
+	"context"
+	"log"
+	"net/http"
+)
+
+// Сервер GoNews.
+type server struct {
+	db  storage.Interface
+	api *api.API
+}
+
+func main() {
+	// Создаём объект сервера.
+	var srv server
+	conString := "mongodb://localhost:2717/"
+	db, err := mongodb.New(conString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Освобождаем ресурсы по завершению приложения
+	defer db.Client.Disconnect(context.Background())
+	// Инициализируем хранилище сервера конкретной БД.
+	srv.db = db
+	// Создаём объект API и регистрируем обработчики.
+	srv.api = api.New(srv.db)
+	// Запускаем веб-сервер на порту 8080 на всех интерфейсах.
+	// Предаём серверу маршрутизатор запросов,
+	// поэтому сервер будет все запросы отправлять на маршрутизатор.
+	// Маршрутизатор будет выбирать нужный обработчик.
+	http.ListenAndServe(":8080", srv.api.Router())
+}
---
+++ cmd/server_psql/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"GoNews/pkg/api"
+	"GoNews/pkg/storage"
+	"GoNews/pkg/storage/postgres"
+	"log"
+	"net/http"
+	"os"
+)
+
+// Сервер GoNews.
+type server struct {
+	db  storage.Interface
+	api *api.API
+}
+
+func main() {
+	// Создаём объект сервера.
+	var srv server
+	// Реляционная БД PostgreSQL.
+	pwd := os.Getenv("postgrespass")
+	conString := "postgres://postgres:" + pwd + "@localhost:5432/tasks?sslmode=disable"
+	db, err := postgres.New(conString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Инициализируем хранилище сервера конкретной БД.
+	srv.db = db
+	// Создаём объект API и регистрируем обработчики.
+	srv.api = api.New(srv.db)
+	// Запускаем веб-сервер на порту 8080 на всех интерфейсах.
+	// Предаём серверу маршрутизатор запросов,
+	// поэтому сервер будет все запросы отправлять на маршрутизатор.
+	// Маршрутизатор будет выбирать нужный обработчик.
+	http.ListenAndServe(":8080", srv.api.Router())
+}
