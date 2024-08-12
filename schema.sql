ace831ebe23d1c366fa39c856ef1836d9c5c70d5
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-08-12 16:55:37 +0500

    Из скрипта удалена генерация тестовых данных, теперь все в медодах пакета postgres.

--- schema.sql
+++ schema.sql
@@ -7,12 +7,9 @@ CREATE TABLE authors (
 
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
-    author_id INTEGER REFERENCES authors(id) NOT NULL,
     title TEXT  NOT NULL,
     content TEXT NOT NULL,
+    author_id INTEGER REFERENCES authors(id) NOT NULL,
     created_at BIGINT DEFAULT extract(epoch from now()),
     published_at BIGINT DEFAULT 0
 );
-
-INSERT INTO authors (name) VALUES ('Дмитрий'),('Владимир'),('Николай');
-INSERT INTO posts (author_id, title, content) VALUES (1 'Статья', 'Содержание статьи');
