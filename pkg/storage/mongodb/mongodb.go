b3461abd4b1f5c08d18e67068fde835fbfb45525
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-08-13 15:18:20 +0500

    Добавлены константы для имен БД и коллекций. В структуру хранилища добавлен мьютекс и счетчики для индексов коллекций. Добавлена структура коллекции авторов. Исправлен конструктор структуры хранилища. Заглушка для метода генерации тестовых данных. Добавлен метод для добавления нового автора в коллекцию.

--- pkg/storage/mongodb/mongodb.go
+++ pkg/storage/mongodb/mongodb.go
@@ -3,16 +3,34 @@ package mongodb
 import (
 	"GoNews/pkg/storage"
 	"context"
+	"sync"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Конструктор объекта хранилища.
+// Константы с именем БД и коллекций
+const (
+	database = "data"
+	posts    = "posts"
+	authors  = "authors"
+)
+
+// Структура объекта хранилища.
 type Store struct {
-	Client *mongo.Client
+	Client       *mongo.Client
+	lastPostID   int
+	lastAuthorID int
+	idMute       *sync.Mutex
 }
 
+// Дополнительная струтура для работы с коллекцией авторов
+type author struct {
+	ID   int `bson:"_id"`
+	name string
+}
+
+// Конструктор, принимает строку подключения к БД.
 func New(conStr string) (*Store, error) {
 	mongoOpts := options.Client().ApplyURI(conStr)
 	client, err := mongo.Connect(context.Background(), mongoOpts)
@@ -24,9 +42,17 @@ func New(conStr string) (*Store, error) {
 		return &Store{}, err
 	}
 	s := Store{
-		Client: client,
+		Client:       client,
+		lastPostID:   0,
+		lastAuthorID: 0,
+		idMute:       &sync.Mutex{},
 	}
 	return &s, nil
+}
+
+// Генератор тестовых данных
+func (s *Store) TestData() error {
+	return nil
 }
 func (s *Store) Posts() ([]storage.Post, error) {
 	var posts []storage.Post
@@ -41,4 +67,19 @@ func (s *Store) UpdatePost(storage.Post) error {
 }
 func (s *Store) DeletePost(storage.Post) error {
 	return nil
+}
+
+// Добавление нового автора в хранилище
+func (s *Store) AddAuthor(name string) (int, error) {
+	s.idMute.Lock()
+	defer s.idMute.Unlock()
+	s.lastAuthorID++
+	colAuthors := s.Client.Database(database).Collection(authors)
+	doc := author{ID: s.lastAuthorID, name: name}
+	result, err := colAuthors.InsertOne(context.TODO(), doc)
+	if err != nil {
+		s.lastAuthorID--
+		return 0, err
+	}
+	return result.InsertedID.(int), nil
 }
