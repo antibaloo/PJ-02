6c12ee8c37e461a905e2a8a7030ba69722b435cb
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-08-14 17:05:23 +0500

    Добавена нотация bson для базовой структуры.

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -3,12 +3,12 @@ package storage
 // Post - публикация.
 type Post struct {
 	ID          int    `json:"id" bson:"_id"` //добавлена нотация bson для работы с mongodb
-	Title       string `json:"title"`
-	Content     string `json:"content"`
-	AuthorID    int    `json:"author_id"`
-	AuthorName  string `json:"author_name"`
-	CreatedAt   int64  `json:"created_at"`
-	PublishedAt int64  `json:"published_at"`
+	Title       string `json:"title" bson:"title"`
+	Content     string `json:"content" bson:"content"`
+	AuthorID    int    `json:"author_id" bson:"author_id"`
+	AuthorName  string `json:"author_name" bson:"-"`
+	CreatedAt   int64  `json:"created_at" bson:"created_at"`
+	PublishedAt int64  `json:"published_at" bson:"published_at"`
 }
 
 // Interface задаёт контракт на работу с БД.
